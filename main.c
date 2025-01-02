2cd07c568b4f4658bf28e14dc4f36f6671ce4339
ayush-palG <ayushpal.9470@gmail.com>
2025-01-02 10:47:13 +0530

    Introduce the rendering of grids in the window

--- main.c
+++ main.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include <SDL2/SDL.h>
 
-#define SCREEN_WIDTH  800
-#define SCREEN_HEIGHT 800
+#define SCREEN_WIDTH  400
+#define SCREEN_HEIGHT 600
 
-#define BACKGROUND_COLOR 0x181818FF
-#define RECT_COLOR       0xFFFFFFFF
+#define CELL_WIDTH  40
+#define CELL_HEIGHT 40
+
+#define BOARD_WIDTH  ((float) SCREEN_WIDTH  / CELL_WIDTH)
+#define BOARD_HEIGHT ((float) SCREEN_HEIGHT / CELL_HEIGHT)
+
+#define BACKGROUND_COLOR     0x181818FF
+#define RECT_COLOR           0xFFFFFFFF
+#define RECT_BOUNDARY_COLOR  0x666666FF
+#define GRID_COLOR           0x444444FF
+#define BLOCK_COLOR          0x46F46FFF
+#define BLOCK_BOUNDARY_COLOR 0x30B13DFF
 
 #define HEX_COLOR(hex)                 \
   ((hex) >> (3*8)) & 0xFF,	       \
   ((hex) >> (2*8)) & 0xFF,	       \
   ((hex) >> (1*8)) & 0xFF,             \
   ((hex) >> (0*8)) & 0xFF
+
+typedef enum {
+  DIR_RIGHT = 0,
+  DIR_UP,
+  DIR_LEFT,
+  DIR_BOTTOM,
+} Dir;
+
+typedef struct {
+  int x, y;
+} Coord;
+
+typedef struct {
+  Coord coords[4];
+} Block;
+
+Coord dirs[4] = {
+  {1,  0},
+  {0, -1},
+  {-1, 0},
+  {0,  1},
+};
 
 void secc(int code)
 {
@@ -32,8 +65,23 @@ void *secp(void *ptr)
   return ptr;
 }
 
+void render_grids(SDL_Renderer *renderer)
+{
+  secc(SDL_SetRenderDrawColor(renderer, HEX_COLOR(GRID_COLOR)));
+
+  for (size_t x = 0; x < BOARD_WIDTH; ++x) {
+    secc(SDL_RenderDrawLine(renderer, x*CELL_WIDTH, 0, x*CELL_WIDTH, SCREEN_HEIGHT));
+  }
+  
+  for (size_t y = 0; y < BOARD_HEIGHT; ++y) {
+    secc(SDL_RenderDrawLine(renderer, 0, y*CELL_HEIGHT, SCREEN_WIDTH, y*CELL_HEIGHT));
+  }
+}
+
 int main(int argc, char *argv[])
 {
+  srand(time(0));
+  
   secc(SDL_Init(SDL_INIT_VIDEO));
 
   SDL_Window *window = secp(SDL_CreateWindow("Tetrissy",
@@ -61,6 +109,7 @@ int main(int argc, char *argv[])
     secc(SDL_SetRenderDrawColor(renderer, HEX_COLOR(RECT_COLOR)));
     SDL_Rect rect = {20, 20, 100, 100};
     secc(SDL_RenderFillRect(renderer, &rect));
+    render_grids(renderer);
     
     SDL_RenderPresent(renderer);
   }
